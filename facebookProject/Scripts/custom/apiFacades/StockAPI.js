faef7875da143b8a880298c12d8e25b47b6d52d3
Robert Gilmore <Robert.Warren.Gilmore@gmail.com>
2015-03-08 21:03:50 -0400

    Added test stock API stuff

--- facebookProject/Scripts/custom/apiFacades/StockAPI.js
+++ facebookProject/Scripts/custom/apiFacades/StockAPI.js
@@ -1,1 +1,17 @@
-﻿
+﻿$.get(
+    'http://dev.markitondemand.com/Api/v2/Quote/jsonp',
+    {symbol: 'GOOG'},
+    function(data) {
+        var lastPrice = Number(data.LastPrice);
+        var change = Number(data.Change);
+        var changeSign;
+        if (change > 0) {
+            changeSign = 1;
+        } else if (change < 0) {
+            changeSign = -1;
+        } else {
+            changeSign = 0;
+        }
+        console.log(data);
+    }
+);
