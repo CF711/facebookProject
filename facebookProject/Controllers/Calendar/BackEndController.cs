0a27bf865d06c73e8b2d366b355ae00644c4ded9
Chris <celdridge91190@gmail.com>
2015-04-01 12:59:29 -0400

    Got page views working and added ability to switch between views on one page

---
+++ facebookProject/Controllers/Calendar/BackEndController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DayPilot.Web.Mvc;
+using DayPilot.Web.Mvc.Data;
+using DayPilot.Web.Mvc.Enums;
+using DayPilot.Web.Mvc.Events.Calendar;
+
+namespace facebookProject.Controllers.Calendar
+{
+    public class BackEndController : Controller
+    {
+        //
+        // GET: /BackEnd/
+
+        public ActionResult Month()
+        {
+            return new Dpm().CallBack(this);
+        }
+
+        public ActionResult Week()
+        {
+            Dpc dpc = new Dpc();
+            dpc.HeaderDateFormat = new DateTime().ToString("dddd");
+            return dpc.CallBack(this);
+        }
+
+        public ActionResult Day()
+        {
+            Dpc dpc = new Dpc();
+            dpc.HeaderDateFormat = "dddd";
+            return dpc.CallBack(this);
+        }
+
+        class Dpc : DayPilotCalendar
+        {
+            public Dpc()
+            {
+                this.HeaderDateFormat = "dddd";
+            }
+            protected override void OnInit(InitArgs e)
+            {
+                Update(CallBackUpdateType.Full);
+
+            }
+
+
+
+            //protected override void OnEventResize(EventResizeArgs e)
+            //{
+            //    new EventManager().EventMove(e.Id, e.NewStart, e.NewEnd);
+            //    Update();
+            //}
+
+            //protected override void OnEventMove(EventMoveArgs e)
+            //{
+            //    new EventManager().EventMove(e.Id, e.NewStart, e.NewEnd);
+            //    Update();
+            //}
+
+            //protected override void OnTimeRangeSelected(TimeRangeSelectedArgs e)
+            //{
+            //    new EventManager().EventCreate(e.Start, e.End, "New event");
+            //    Update();
+            //}
+
+            //protected override void OnBeforeEventRender(BeforeEventRenderArgs e)
+            //{
+            //    e.Areas.Add(new Area().Right(3).Top(3).Width(15).Height(15).CssClass("event_action_delete").JavaScript("switcher.active.control.commandCallBack('delete', {'e': e});"));
+            //}
+
+            //protected override void OnCommand(CommandArgs e)
+            //{
+            //    switch (e.Command)
+            //    {
+            //        case "navigate":
+            //            StartDate = (DateTime)e.Data["day"];
+            //            Update(CallBackUpdateType.Full);
+            //            break;
+            //        case "refresh":
+            //            Update(CallBackUpdateType.EventsOnly);
+            //            break;
+            //        case "delete":
+            //            new EventManager().EventDelete((string)e.Data["e"]["id"]);
+            //            Update(CallBackUpdateType.EventsOnly);
+            //            break;
+            //    }
+            //}
+
+            //protected override void OnFinish()
+            //{
+            //    if (UpdateType == CallBackUpdateType.None)
+            //    {
+            //        return;
+            //    }
+
+            //    Events = new EventManager().FilteredData(StartDate, StartDate.AddDays(Days)).AsEnumerable();
+
+            //    DataIdField = "id";
+            //    DataTextField = "name";
+            //    DataStartField = "eventstart";
+            //    DataEndField = "eventend";
+            //}
+        }
+
+        class Dpm : DayPilotMonth
+        {
+            protected override void OnInit(DayPilot.Web.Mvc.Events.Month.InitArgs e)
+            {
+                Update();
+            }
+
+            //protected override void OnEventResize(DayPilot.Web.Mvc.Events.Month.EventResizeArgs e)
+            //{
+            //    new EventManager().EventMove(e.Id, e.NewStart, e.NewEnd);
+            //    Update();
+            //}
+
+            //protected override void OnEventMove(DayPilot.Web.Mvc.Events.Month.EventMoveArgs e)
+            //{
+            //    new EventManager().EventMove(e.Id, e.NewStart, e.NewEnd);
+            //    Update();
+            //}
+
+            //protected override void OnTimeRangeSelected(DayPilot.Web.Mvc.Events.Month.TimeRangeSelectedArgs e)
+            //{
+            //    new EventManager().EventCreate(e.Start, e.End, "New event");
+            //    Update();
+            //}
+
+
+            //protected override void OnCommand(DayPilot.Web.Mvc.Events.Month.CommandArgs e)
+            //{
+            //    switch (e.Command)
+            //    {
+            //        case "navigate":
+            //            StartDate = (DateTime)e.Data["day"];
+            //            Update(CallBackUpdateType.Full);
+            //            break;
+            //        case "refresh":
+            //            Update(CallBackUpdateType.EventsOnly);
+            //            break;
+            //        case "delete":
+            //            new EventManager().EventDelete((string)e.Data["e"]["id"]);
+            //            Update(CallBackUpdateType.EventsOnly);
+            //            break;
+            //    }
+            //}
+
+            //protected override void OnFinish()
+            //{
+            //    if (UpdateType == CallBackUpdateType.None)
+            //    {
+            //        return;
+            //    }
+
+            //    Events = new EventManager().FilteredData(VisibleStart, VisibleEnd).AsEnumerable();
+
+            //    DataIdField = "id";
+            //    DataTextField = "name";
+            //    DataStartField = "eventstart";
+            //    DataEndField = "eventend";
+            //}
+        }
+
+    }
+}
---
+++ facebookProject/Controllers/Calendar/CalendarController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace NoseBook.Controllers
+{
+    public class CalendarController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+
+        }
+
+        public ActionResult WeekView()
+        {
+            return View();
+        }
+
+        public void displayDayView()
+        {
+            Console.WriteLine("Called displayDayView");
+        }
+
+        public void displayWeekView()
+        {
+
+            Console.WriteLine("Called displayWeekView");
+        }
+
+        public void displayMonthView()
+        {
+
+            Console.WriteLine("Called displayMonthView");
+        }
+
+
+    }
+}
